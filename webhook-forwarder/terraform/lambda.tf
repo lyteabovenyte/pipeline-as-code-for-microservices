374ae7d488b46e4b63b15da3b4210d71c79a8959
lyteabovenyte <lyteabovenyte@gmail.com>
2024-07-21 11:11:21 +0330

    added comment on lambda function entrypoint on index.js

--- webhook-forwarder/terraform/lambda.tf
+++ webhook-forwarder/terraform/lambda.tf
@@ -1,5 +1,5 @@
 resource "aws_lambda_function" "lambda" {
-  filename      = "../deployment.zip"
+  filename      = "../deployment.zip" # packaging index.js entrypoint for the lambda function.
   function_name = "GitHubWebhookForwarder"
   role          = aws_iam_role.role.arn
   handler       = "index.handler"
