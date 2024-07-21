928074040dfa258035045979d8516f451ff49179
lyteabovenyte <lyteabovenyte@gmail.com>
2024-07-21 10:31:15 +0330

    formating terraform files

--- webhook-forwarder/terraform/apigateway.tf
+++ webhook-forwarder/terraform/apigateway.tf
@@ -1,24 +1,24 @@
 resource "aws_api_gateway_rest_api" "api" {
-  name = "GitHubWebHookAPI"
+  name        = "GitHubWebHookAPI"
   description = "GitHub Webhook forwarder"
 }
 
 resource "aws_api_gateway_resource" "path" {
   rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id = aws_api_gateway_rest_api.api.root_resource_id
-  path_part = "webhook"
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "webhook"
 }
 
 resource "aws_api_gateway_integration" "request_integration" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_method.request_method.resource_id
   http_method = aws_api_gateway_method.request_method.http_method
-  type = "AWS_PROXY"
-  uri = aws_lambda_function.lambda.invoke_arn
+  type        = "AWS_PROXY"
+  uri         = aws_lambda_function.lambda.invoke_arn
 }
 
 # activate the config and expose the API at a URL
 resource "aws_api_gateway_deployment" "stage" {
   rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name = "v1"
+  stage_name  = "v1"
 }
--- webhook-forwarder/terraform/lambda.tf
+++ webhook-forwarder/terraform/lambda.tf
@@ -1,13 +1,13 @@
 resource "aws_lambda_function" "lambda" {
-  filename = "../deployment.zip"
+  filename      = "../deployment.zip"
   function_name = "GitHubWebhookForwarder"
-  role = aws_iam_role.role.arn
-  handler = "index.handler"
-  runtime = "nodejs14.x"
-  timeout = 10
+  role          = aws_iam_role.role.arn
+  handler       = "index.handler"
+  runtime       = "nodejs14.x"
+  timeout       = 10
   environment {
     variables = {
-        JENKINS_URL = var.jenkins_url
+      JENKINS_URL = var.jenkins_url
     }
   }
 }
